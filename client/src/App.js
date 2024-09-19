c95ff64342b21decdc944e6662e028f566c00098
TKpepper15 <tejjas15@gmail.com>
2024-09-18 23:34:55 -0400

    fixed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { ObjectRecognitionProvider } from './contexts/ObjectRecognitionContext';
 import Home from './pages/Home';
@@ -7,7 +7,7 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import 'Smart-Vision/client/src/App.js';
+import './styles/App.css'; // Correct path for CSS
 
 const App = () => {
     return (
@@ -17,12 +17,12 @@ const App = () => {
                     <div className="App">
                         <Header />
                         <main>
-                            <Switch>
-                                <Route path="/" exact component={Home} />
-                                <Route path="/about" component={About} />
-                                <Route path="/contact" component={Contact} />
+                            <Routes>
+                                <Route path="/" element={<Home />} />
+                                <Route path="/about" element={<About />} />
+                                <Route path="/contact" element={<Contact />} />
                                 {/* Add more routes as needed */}
-                            </Switch>
+                            </Routes>
                         </main>
                         <Footer />
                     </div>
