3a556c8610ffb0a514786721a07181f1f62d4c3d
Vishwak <vishwaksena.velamuri@gmail.com>
2024-09-20 13:48:25 -0400

    removed hazard stuff

--- server/app/services/hazard_detection_service.py
+++
@@ -1,7 +0,0 @@
-class HazardDetectionService:
-    def detect_hazard(self, data):
-        # Placeholder logic for detecting hazards
-        hazards = []
-        if 'obstacle' in data:
-            hazards.append({'type': 'obstacle', 'location': data['obstacle']})
-        return hazards
